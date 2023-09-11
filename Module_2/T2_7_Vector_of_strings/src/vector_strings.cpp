af8cad9bdf28c3120c0559a0cfe12b74a5ddeb18
Harsh Rathee <harsh.rathee@aalto.fi>
2023-09-11 23:14:07 +0300

    2.7

--- Module_2/T2_7_Vector_of_strings/src/vector_strings.cpp
+++ Module_2/T2_7_Vector_of_strings/src/vector_strings.cpp
@@ -1,20 +1,40 @@
 #include "vector_strings.hpp"
 
 #include <iostream>
+#include <string>
 #include <vector>
 
 void Adder(std::vector<std::string>& names) {
-  
+  std::cout << "Enter a name:" << std::endl;
+  std::string name;
+  std::cin >> name;
+  names.push_back(name);
+  std::cout << "Number of names in the vector:" << std::endl
+            << names.size() << std::endl;
 }
 
 void Remover(std::vector<std::string>& names) {
-  
+  std::string name = names[names.size() - 1];
+  names.pop_back();
+  std::cout << "Removing the last element:" << std::endl << name << std::endl;
 }
 
-void Printer(std::vector<std::string>& names) {
-  
-}
+void Printer(std::vector<std::string>& names) {}
 
 void CMDReader() {
-  
+  std::vector<std::string> names;
+  while (true) {
+    std::cout << "Commands: ADD, PRINT, REMOVE, QUIT" << std::endl;
+    std::string command;
+    std::cin >> command;
+    if (command == "ADD") {
+      Adder(names);
+    } else if (command == "PRINT") {
+      Printer(names);
+    } else if (command == "REMOVE") {
+      Remover(names);
+    } else if (command == "QUIT") {
+      return;
+    }
+  }
 }
