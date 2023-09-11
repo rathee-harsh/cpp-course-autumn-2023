a78170c18fea20aaffe618d50addb523ed008aa5
Harsh Rathee <harsh.rathee@aalto.fi>
2023-09-11 23:04:03 +0300

    2.6

--- Module_2/T2_6_Vectors/src/vector.cpp
+++ Module_2/T2_6_Vectors/src/vector.cpp
@@ -3,16 +3,32 @@
 #include <vector>
 
 int GetMin(std::vector<int> v) {
-  
-  return 0; 
+  int smallest = 1000;
+  for (unsigned int i = 0; i < v.size(); i++) {
+    if (v[i] < smallest) {
+      smallest = v[i];
+    }
+  }
+  return smallest;
 }
 
 int GetMax(std::vector<int> v) {
-  
-  return 0; 
+  int largest = -1000;
+  for (unsigned int i = 0; i < v.size(); i++) {
+    if (v[i] > largest) {
+      largest = v[i];
+    }
+  }
+  return largest;
 }
 
 double GetAvg(std::vector<int> v) {
-  
-  return 0.0; 
+  if (v.size() == 0) {
+    return 0;
+  }
+  int sum = 0;
+  for (unsigned int i = 0; i < v.size(); i++) {
+    sum += v[i];
+  }
+  return 1.0 * sum / v.size();
 }
