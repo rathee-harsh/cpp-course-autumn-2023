78967dfb34e89d13690fe14fe8ae074fb6914886
Harsh Rathee <harsh.rathee@aalto.fi>
2023-09-11 22:29:28 +0300

    2.3

--- Module_2/T2_3_Simple_class/src/person.cpp
+++ Module_2/T2_3_Simple_class/src/person.cpp
@@ -2,3 +2,9 @@
 
 // define your Person class' functions here
 
+Person::Person(const std::string& name, int birthyear)
+    : name_(name), birthyear_(birthyear) {}
+
+const std::string& Person::GetName() const { return name_; }
+
+int Person::GetAge(int year) const { return year - birthyear_; }
--- Module_2/T2_3_Simple_class/src/person.hpp
+++ Module_2/T2_3_Simple_class/src/person.hpp
@@ -1,7 +1,18 @@
+#include <string>
+
 #ifndef AALTO_ELEC_CPP_PERSON
 #define AALTO_ELEC_CPP_PERSON
 
 // declare your Person class here
+class Person {
+ public:
+  Person(const std::string& name, int birthyear);
+  const std::string& GetName() const;
+  int GetAge(int year) const;
 
+ private:
+  std::string name_;
+  int birthyear_;
+};
 
 #endif
