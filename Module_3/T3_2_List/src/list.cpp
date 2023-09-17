b137121abe28c3058d97c28fe76a6a09dd4a2273
Harsh Rathee <harsh.rathee@aalto.fi>
2023-09-17 23:14:11 +0300

    3.2

--- Module_3/T3_2_List/src/list.cpp
+++ Module_3/T3_2_List/src/list.cpp
@@ -1,17 +1,31 @@
 #include "list.hpp"
 
+#include <array>
 #include <iostream>
+#include <sstream>
 
 // Implement the functions here
 
 std::istream& GetLines(std::istream& is, std::list<std::string>& list) {
-  
+  list.clear();
+  for (char line[100]; is.getline(line, 100);) {
+    std::string line_str(line);
+    list.push_back(line_str);
+  }
+  if (list.back() == "\n") {
+    list.pop_back();
+  }
+  return is;
 }
 
 void Print(const std::list<std::string>& list) {
-  
+  for (auto line : list) {
+    std::cout << line;
+  }
+  std::cout << std::endl;
 }
 
 void SortAndUnique(std::list<std::string>& list) {
-  
+  list.sort();
+  list.unique();
 }
