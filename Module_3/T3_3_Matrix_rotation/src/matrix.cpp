c25cdd6e97f3977862727c1c5a712ee6aa50f654
Harsh Rathee <harsh.rathee@aalto.fi>
2023-09-17 23:39:52 +0300

    3.3

--- Module_3/T3_3_Matrix_rotation/src/matrix.cpp
+++ Module_3/T3_3_Matrix_rotation/src/matrix.cpp
@@ -1,5 +1,6 @@
 #include "matrix.hpp"
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -8,10 +9,43 @@
 
 // Assumes 'm' is a square matrix
 Matrix ReadMatrix(int n) {
+  Matrix matrix;
+  for (int i = 0; i < n; i++) {
+    std::vector<int> new_row;
+    for (int j = 0; j < n; j++) {
+      int e;
+      std::cin >> e;
+      new_row.push_back(e);
+    }
+    matrix.push_back(new_row);
   }
+  return matrix;
+}
 
 Matrix Rotate90Deg(const Matrix &m) {
+  Matrix r;
+  for (std::vector<std::vector<int>>::size_type i = 0; i < m.size(); i++) {
+    std::vector<int> row(m.size());
+    r.push_back(row);
   }
+  for (std::vector<std::vector<int>>::size_type i = 0; i < m.size(); i++) {
+    for (std::vector<std::vector<int>>::size_type j = 0; j <= i; j++) {
+      r[j][i] = m[i][j];
+      r[i][j] = m[j][i];
+    }
+  }
+  for (std::vector<std::vector<int>>::size_type i = 0; i < m.size(); i++) {
+    std::reverse(r[i].begin(), r[i].end());
+  }
+}
 
 void Print(const Matrix &m) {
+  std::cout << "Printing out a " << m.size() << " x " << m.size()
+            << " matrix:" << std::endl;
+  for (auto row : m) {
+    for (auto e : row) {
+      std::cout << e << " ";
+    }
+    std::cout << std::endl;
   }
+}
